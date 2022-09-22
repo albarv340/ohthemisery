f0a96f7f3e28a2c2efd4e6d573d96384b0bba850
albarv340 <54350887+albarv340@users.noreply.github.com>
2022-09-22 19:58:08 +0200

    url preview test

--- pages/builder.js
+++ pages/builder.js
@@ -86,7 +86,8 @@ export default function Builder({ build }) {
         <div className="container-fluid">
             <Head>
                 <title>Monumenta Builder</title>
-                <meta name="description" content="Monumenta builder." />
+                <meta property="og:site_name" content="OHTHEMISERY.TK" />
+                <meta name="description" content={`Monumenta builder.${(build ? `\n${build.split(/\&.=/)?.join("\n")?.substr(2)}` : "")}`} />
                 <meta name="keywords" content="Monumenta, Minecraft, MMORPG, Items, Builder" />
             </Head>
             <HomeButton />
